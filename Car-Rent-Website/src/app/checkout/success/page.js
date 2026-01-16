fc50ab3dd2fedd0bb2bdcc379183e8b21df6813b
Arkadiusz Asmus <arekas2914@wp.pl>
2026-01-16 19:15:36 +0100

    button color change

--- Car-Rent-Website/src/app/checkout/success/page.js
+++ Car-Rent-Website/src/app/checkout/success/page.js
@@ -25,7 +25,7 @@ export default function CheckoutSuccessPage() {
         <div className="space-y-3">
           <Button
             onClick={() => router.push("/offer")}
-            className="w-full bg-blue-600 text-white py-3 rounded-lg font-semibold hover:bg-blue-700"
+            className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800"
           >
             Continue Shopping
           </Button>
